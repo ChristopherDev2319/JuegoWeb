83bce7e0b44046fe28b765551c903943b952f103
Chris <moralesparedeschristopher25@gmail.com>
2026-01-07 02:14:15 -0600

    chore: remove cluster system - delete server/cluster folder and clean references

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 /**
  * WebSocket Server Entry Point
  * Main server for multiplayer FPS game
- * 
- * Uses legacy single-process server for stability.
- * Cluster mode available in fix/new-features branch.
  */
 
 import { startLegacyServer } from './legacyServer.js';
